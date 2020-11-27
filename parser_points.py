2c044af2d106f456feb0ad3f9e1d7b27d220c60b
Maksim Provotorov <provotorov_maksim@mail.ru>
2020-11-27 20:42:09 +0300

    fix error. Filter vector_x

--- parser_points.py
+++ parser_points.py
@@ -11,6 +11,7 @@ def parse_file_points(filename):
         y_str = utils.remove_char(vectors[1]).split(',')
         y = utils.str_to_list(y_str)
         #Если вектор содержит меньше 5 элементов, или их значения не равны, то пропускаем
+        x = quicksort(x)
         if len(x) != len(y):
             continue
         list_points_parse.append([x, y])
@@ -18,3 +19,12 @@ def parse_file_points(filename):
     return list_points_parse
 
 
+
+def quicksort(array):
+    if len(array) <= 1:  #Базовый случай
+        return array
+    else:  #Рекурсивный случай
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]  #Подмассив всех элементов меньше опорного
+        greater = [i for i in array[1:] if i > pivot]  #Подмассив всех элементов больше опорного
+        return quicksort(less) + [pivot] + quicksort(greater)
