6f34698a54b3edfb9dc735bdb0a0830ac3a8c001
Maksim Provotorov <provotorov_maksim@mail.ru>
2020-12-15 21:32:57 +0300

    Исправил ошибки при подсчете мат ожидания

--- labs_4/main.py
+++ labs_4/main.py
@@ -1,6 +1,6 @@
 from labs_4.create_sampling import *
 from labs_4.graphs_points import create_histogram, create_polygon
-from labs_4.ploting import plot_histogram, plot_polygon
+from labs_4.ploting import plot_histogram, plot_polygon, plot_hist_polygon
 from labs_4.math_utils import *
 from labs_4.sampling_value import *
 
@@ -13,14 +13,15 @@ data_hist_normal, k_normal = create_histogram(x_normal, interval_normal)
 data_polygon_normal, kp_normal = create_polygon(x_normal, interval_normal)
 
 plot_histogram(x_normal, data_hist_normal, k_normal, interval_normal, "Равномерное распределение", 1)
-plot_polygon(x_normal, data_polygon_normal, kp_normal, interval_normal, "Равномерное распределение", 1)
+plot_polygon(x_normal, data_polygon_normal, interval_normal, "Равномерное распределение", 1)
 
 print("////////////////////////////////////////////////////////////////////////////////")
 mn = find_mathematical_expectation(x_normal)
 print("Математическое ожидание равномерного распределения: ", (b + a) / 2)
 print("Дисперсия равномерного распределения", (b - a) ** 2 / 12)
 print("Выборочное математическое ожидание равномерного распределения: ", mn)
-print("Выборочная дисперсия равномерного распределения", find_dispersion(x_normal, mn))
+print("Выборочная дисперсия равномерного распределения c известным МО", find_dispersion(x_normal, (b + a) / 2))
+print("Выборочная дисперсия равномерного распределения c неизвестным МО", find_dispersion(x_normal, mn))
 
 print("_____________________________________________________________________________________")
 # 1.2 Формируем выборку велечин, распредленных по Гауссовскому закону с параметрами m и D
@@ -30,13 +31,16 @@ print("/////////////////////////////////////////////////////////////////////////
 x_gauss, interval_gauss = create_gauss_sampling(m_gauss, d_gauss)
 data_hist_gauss, k_gauss = create_histogram(x_gauss, interval_gauss)
 data_polygon_gauss, kp_gauss = create_polygon(x_gauss, interval_gauss)
+
 plot_histogram(x_gauss, data_hist_gauss, k_gauss, interval_gauss, "Распределение Гаусса", 2, m=m_gauss, d=d_gauss)
-plot_polygon(x_gauss, data_polygon_gauss, kp_gauss, interval_gauss, "Распределение Гаусса", 2, m=m_gauss, d=d_gauss)
+plot_polygon(x_gauss, data_polygon_gauss, interval_gauss, "Распределение Гаусса", 2, m=m_gauss, d=d_gauss)
+
 print("Математчисекое ожидание распределения Гаусса: ", m_gauss)
 print("Дисперсия распределения Гаусса", d_gauss)
 mv = find_mathematical_expectation(x_gauss)
 print("Выборочное математическое ожидание распределения Гаусса: ", mv)
-print("Выборочная дисперсия ожидание распределения Гаусса: ", find_dispersion(x_gauss, mv))
+print("Выборочная дисперсия c известным МО распределения Гаусса: ", find_dispersion(x_gauss, m_gauss))
+print("Выборочная дисперсия c неизвестным МО распределения Гаусса: ", find_dispersion(x_gauss, mv))
 print("_____________________________________________________________________________________")
 
 # 1.3   Осууществим выборку случайных значенй по методу Неймана
@@ -49,11 +53,15 @@ data_polygon_neumann, kp_neumann = create_polygon(x_neumann, interval_neumann)
 
 plot_histogram(x_neumann, data_hist_neumann, k_neumann, interval_neumann, "Распределение Релея", 3,
                sigma=sigma_rayleigh)
-plot_polygon(x_neumann, data_polygon_neumann, kp_neumann, interval_neumann, "Распределение Релея", 3,
+plot_polygon(x_neumann, data_polygon_neumann, interval_neumann, "Распределение Релея", 3,
              sigma=sigma_rayleigh)
 
-print("Математическое ожидание распрееделение Рэле ", math.sqrt((math.pi * (sigma_rayleigh ** 2)) / 2))
+m_ne = math.sqrt((math.pi * (sigma_rayleigh ** 2)) / 2)
+print("Математическое ожидание распрееделение Рэле ", m_ne)
 print("Дисперсия распределения Рэле: ", (2 - math.pi / 2) * (sigma_rayleigh ** 2))
 mk = find_mathematical_expectation(x_neumann)
 print("Выборочное математическое ожидание распределения Рэле: ", mk)
-print("Выборочная дисперсия распределения Рэле: ", find_dispersion(x_neumann, mk))
+print("Выборочная дисперсия распределения Рэле с известным МО: ", find_dispersion(x_neumann, m_ne))
+print("Выборочная дисперсия распределения Рэле с неизвестным МО: ", find_dispersion(x_neumann, mk))
+
+#plot_hist_polygon(data_polygon_neumann, data_hist_neumann, k_neumann, interval_neumann, "Экспиременте. Распределение Релея")
--- labs_4/ploting.py
+++ labs_4/ploting.py
@@ -17,17 +17,38 @@ def plot_histogram(X, data_hist, k, interval, title, dstr, m=0, d=0, sigma=0):
     plt.show()
 
 
-def plot_polygon(X, data_polygon, k, interval, title, dstr, m=0, d=0, sigma=0):
+def plot_polygon(X, data_polygon, interval, title, dstr, m=0, d=0, sigma=0):
     a = interval[0]
     b = interval[1]
-    x = []
-    y = []
+    x = [a]
+    y = [0]
     for i in data_polygon:
         y.append(i[0])
         x.append(i[1])
         plt.scatter(x, y)
-    plt.plot(x, y)
+    plt.step(x, y)
     plt.title(title)
     # Рисуем функцию распределения(Да, мне лень нормально называть)
     plot_density_f(X, a, b, dstr, m, d, sigma)
+    plt.show()
+
+
+def plot_hist_polygon(data_polygon, data_hist, k, interval, title):
+    a = interval[0]
+    b = interval[1]
+    x = [a]
+    y = [0]
+    delta = (b - a) / k
+    for i in data_polygon:
+        y.append(i[0])
+        x.append(i[1])
+        plt.scatter(x, y)
+    for data in data_hist:
+        x_min = data[1]
+        x_max = data[2]
+        gl = data[0]  # частота
+        rl = (x_max + x_min) / 2
+        plt.bar(rl, gl, width=delta, color="b")
+    plt.step(x, y)
+    plt.title(title)
     plt.show()
